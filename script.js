2128ee9affafea55f287b55f17999caf821c00d3
Igor Tamuz <igortamuz@gmail.com>
2022-06-23 11:46:39 -0300

    Sidebar and Login Area

---
+++ script.js
@@ -0,0 +1,9 @@
+function callSideBar(element){
+    let darkArea = document.querySelector(".darkArea")
+    darkArea.classList.add("darkAreaCall")
+}
+
+function closeSideBar(element){
+    let darkArea = document.querySelector(".darkArea")
+    darkArea.classList.remove("darkAreaCall")
+}
